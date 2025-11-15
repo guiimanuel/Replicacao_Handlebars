0053430d6e0d8e7d380987803b6ac7d0347ee21c
Guilherme Manuel <129343802+guiimanuel@users.noreply.github.com>
2025-11-14 23:08:22 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const exphbs =  require('express-handlebars');
+
+const app = express();
+const port = 8200;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.engine('handlebars', exphbs.engine({defaultLayout: false}));
+app.set('view engine', 'handlebars');
+
+let pessoas = [
+    {id: 1, nome: 'Pessoa 1'},
+    {id: 2, nome: 'Pessoa 2'},
+    {id: 3, nome: 'Pessoa 3'}
+];
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+//listar
+app.get('/pessoas', (req,res) => {
+    res.render('listarPessoas', {pessoas});
+});
+
+//cadastrar
+app.get('/pessoas/nova', (req,res) => {
+    res.render('cadastrarPessoa')
+});
+
+app.post('/pessoas', (req,res) => {
+    const {nome} = req.body;
+    const novaPessoa = {
+        id: pessoas.length + 1,
+        nome: nome
+    };
+    pessoas.push(novaPessoa);
+    res.render('listarPessoas', {pessoas});
+});
+
+//detalhar
+app.get('/pessoas/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const pessoa = pessoas.find(p => p.id === id);
+    if(pessoa){
+        res.render('detalharPessoa', {pessoa});
+    }else{
+        res.status(404).send('Pessoa não encontrada');
+    }
+})
+
+//editar
+app.get('/pessoas/:id/editar', (req,res) => {
+    const id = parseInt(req.params.id);
+    const pessoa = pessoas.find(p => p.id === id);
+
+    if(pessoa) {
+        res.render('editarPessoa', {pessoa});
+    }else{
+        return res.status(404).send('Pessoa não encontrada.');
+}})
+
+app.post('/pessoas/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pessoa = pessoas.find(p => p.id === id);
+
+    if(pessoa){
+        pessoa.nome = req.body.nome;
+        res.render('listarPessoas', {pessoas});
+    }else{
+        return res.status(404).send('Pessoa não encontrada.')
+    }
+});
+
+//excluir
+app.post('/pessoas/:id/excluir', (req,res) => {
+    const id = parseInt(req.params.id)
+    const index = pessoas.findIndex(p=> p.id === id);
+
+    if(index !== -1){
+        pessoas.splice(index, 1);
+        res.render('listarPessoas', {pessoas});
+    }else{
+        return res.status(404).send('Pessoa não encontrada')
+    }
+})
+app.listen(port, ()=>{
+    console.log(`Servidor em execução: http://localhost:${port}`);
+});
